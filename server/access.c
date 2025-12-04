aeed729e8d2ad08ae5c49f52c5cdf0e4ef76a944
Luiggi33 <github@luiggi33.de>
2025-12-04 20:25:37 +0100

    add option to access stanza to respect if-bound mode

--- server/access.c
+++ server/access.c
@@ -1818,6 +1818,8 @@ parse_access_file(fko_srv_options_t *opts, char *access_filename, int *depth)
             add_acc_bool(&(curr_acc->require_source_address), val);
         else if(CONF_VAR_IS(var, "REQUIRE_SOURCE"))  /* synonym for REQUIRE_SOURCE_ADDRESS */
             add_acc_bool(&(curr_acc->require_source_address), val);
+        else if(CONF_VAR_IS(var, "PF_IF_BOUND"))
+            add_acc_bool(&(curr_acc->pf_if_bound), val);
         else if(CONF_VAR_IS(var, "GPG_HOME_DIR"))
         {
             if (is_valid_dir(val))
--- server/fwknopd_common.h
+++ server/fwknopd_common.h
@@ -451,6 +451,10 @@ typedef struct acc_stanza
     char                *force_snat_ip;
     unsigned char        force_masquerade;
 
+    /* PF if-bound mode
+    */
+    unsigned char        pf_if_bound;
+
     struct acc_stanza   *next;
 } acc_stanza_t;
 
